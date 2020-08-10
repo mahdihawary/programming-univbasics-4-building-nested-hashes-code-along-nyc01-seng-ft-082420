d8e9379b7a95e4becb0424ce71225af6958214d1
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-10 03:44:59 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,16 +1,10 @@
 def first_challenge
    epic_tragedy = {
      montague:{
-       patriarch:{},
-       matriarch:{},
-       hero:{},
-       hero_friends:{}
+      
      },
      capulet:{
-       patriarch:{},
-       matriarch:{},
-       hero:{},
-       hero_friends:{}
+      
      }
    }
 end
